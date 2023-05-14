a27c8a69c75d7062bb28502847662b3fd453a257
Drek <doxology@rogers.com>
2023-05-14 15:03:06 -0300

    miscellaneous fixes

--- www/includes/PS/halflife/firearms.php
+++ www/includes/PS/halflife/firearms.php
@@ -47,16 +47,8 @@ function add_map_player_list_mod($map, $setup = array()) {
 	global $cms;
 	$this->add_map_player_list('ffkills',		$setup + array('label' => $cms->trans("Friendly Fire Kills")) );
 	$this->add_map_player_list('bandage',	$setup + array('label' => $cms->trans("Soldiers Bandaged")) );
-	$this->add_map_player_list('suture',	$setup + array('label' => $cms->trans("Soldiers Sutured")) );
-	$this->add_map_player_list('treatconcussion',		$setup + array('label' => $cms->trans("Concussions Treated")) );
 	$this->add_map_player_list('medevac',	$setup + array('label' => $cms->trans("Casualties Medevaced")) );
 	$this->add_map_player_list('capturepoint',	$setup + array('label' => $cms->trans("Areas Captured")) );
-	$this->add_map_player_list('howitzerammo',		$setup + array('label' => $cms->trans("Howizer Ammo Delivered")) );
-	$this->add_map_player_list('targettingpack',	$setup + array('label' => $cms->trans("Targetting Pack Delivered")) );
-	$this->add_map_player_list('redintelligence',	$setup + array('label' => $cms->trans("Red Intel Delivered")) );
-	$this->add_map_player_list('blueintelligence',	$setup + array('label' => $cms->trans("Blue Intel Delivered")) );
-	$this->add_map_player_list('secretdocuments',	$setup + array('label' => $cms->trans("Secret Docs Delivered")) );
-	$this->add_map_player_list('destroyobject',	$setup + array('label' => $cms->trans("Objective Destroyed")) );
 }
 
 // Add or remove columns from maps.php listing
